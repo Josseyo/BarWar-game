27416c2272d77a74a4ec030d92022822ec65a5a7
Josseyo Yoshida <josefin@yomaco.com>
2024-04-18 13:47:28 +0000

    Js: Delete empty rows in eventlistener,
    and in Function play game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,9 @@ const computerDisplay = document.getElementById("computerDisplay");
 
 //Add event listener for button-clicks
 const options = document.querySelectorAll(".button");
-
 options.forEach((option) => {
-
     option.addEventListener("click", function () {
-
         const playerChoice = option.getAttribute("aria-label");
-
         playGame(playerChoice);
 
     });
@@ -31,41 +27,23 @@ function determineWinner(playerChoice, computerChoice) {
         return "It's a tie...";
     } else {
         switch (playerChoice) {
-
             case "rock":
-
                 result = (computerChoice === "scissors" || computerChoice === "lizard") ? "You win!" : "You lose:(";
-
                 break;
-
             case "paper":
-
                 result = (computerChoice === "rock" || computerChoice === "spock") ? "You win!" : "You lose:(";
-
                 break;
-
             case "scissors":
-
                 result = (computerChoice === "paper" || computerChoice === "lizard") ? "You win!" : "You lose:(";
-
                 break;
-
             case "lizard":
-
                 result = (computerChoice === "spock" || computerChoice === "paper") ? "You win!" : "You lose:(";
-
                 break;
-
             case "spock":
-
                 result = (computerChoice === "rock" || computerChoice === "scissors") ? "You win!" : "You lose:(";
-
                 break;
-
             default:
-
                 result = "Invalid choice!";
-
         }
     }
 
