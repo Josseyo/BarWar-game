bb83b90c7ff10657b2bc6f9458c7ed4bb98c3c05
Josseyo Yoshida <josefin@yomaco.com>
2024-04-18 12:26:09 +0000

    js: Add event listener for button-clicks (options)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+const holder = ["rock", "paper", "scissors", "lizard", "spock"];
+//Add event listener for button-clicks
+
+const options = document.querySelectorAll(".button");
+
+options.forEach((option) => {
+
+    option.addEventListener("click", function () {
+
+        const playerChoice = option.getAttribute("aria-label");
+
+        playGame(playerChoice);
+
+    });
+
+});
+
 //Function play game
 function playGame() {
 
