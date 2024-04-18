6fca54a0f3da15e415abf480a7fc1618b4eed7ee
Josseyo Yoshida <josefin@yomaco.com>
2024-04-18 13:11:33 +0000

    js: Add function Determine the winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,9 +25,50 @@ function playGame(playerChoice) {
     display(playerChoice, computerChoice, result);
 }
 
-//Function determine the winner
-function determineWinner() {
+// Function determine the winner
+function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "It's a tie...";
+    } else {
+        switch (playerChoice) {
 
+            case "rock":
+
+                result = (computerChoice === "scissors" || computerChoice === "lizard") ? "You win!" : "You lose:(";
+
+                break;
+
+            case "paper":
+
+                result = (computerChoice === "rock" || computerChoice === "spock") ? "You win!" : "You lose:(";
+
+                break;
+
+            case "scissors":
+
+                result = (computerChoice === "paper" || computerChoice === "lizard") ? "You win!" : "You lose:(";
+
+                break;
+
+            case "lizard":
+
+                result = (computerChoice === "spock" || computerChoice === "paper") ? "You win!" : "You lose:(";
+
+                break;
+
+            case "spock":
+
+                result = (computerChoice === "rock" || computerChoice === "scissors") ? "You win!" : "You lose:(";
+
+                break;
+
+            default:
+
+                result = "Invalid choice!";
+
+        }
+    }
+
+    //Function display result and update scores
+    function display() {}
 }
-//Function display result and update scores
-function display() {}
